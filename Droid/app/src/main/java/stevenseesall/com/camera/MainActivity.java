cdfb4b52a2f90a83ebadb56ce3398fe34c7fc357
elrhino <renlane@hotmail.com>
2015-02-23 13:21:50 -0500

    Suppression de l'option standalone

--- Droid/app/src/main/java/stevenseesall/com/camera/MainActivity.java
+++ Droid/app/src/main/java/stevenseesall/com/camera/MainActivity.java
@@ -17,9 +17,8 @@ public class MainActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_PC).setOnClickListener(new PC_OnClickListener(this));
-        findViewById(R.id.btn_StandAlone).setOnClickListener(new StandAlone_OnClickListener(this));
+        setContentView(R.layout.login);
+        findViewById(R.id.btn_Connect).setOnClickListener(new Connection_OnClickListener(this));
     }
 
     private class Connection_OnClickListener implements View.OnClickListener {
@@ -48,44 +47,6 @@ public class MainActivity extends ActionBarActivity{
             udpRunnable.start();
         }
     }
-
-    private class PC_OnClickListener implements View.OnClickListener {
-
-        Activity mActivity;
-
-        public PC_OnClickListener(Activity activity) {
-            mActivity = activity;
-        }
-
-        @Override
-        public void onClick(View v) {
-            setContentView(R.layout.login);
-            findViewById(R.id.btn_Connect).setOnClickListener(new Connection_OnClickListener(mActivity));
-        }
-    }
-
-    private class StandAlone_OnClickListener implements View.OnClickListener {
-
-        Activity mActivity;
-
-        public StandAlone_OnClickListener(Activity activity) {
-            mActivity = activity;
-        }
-
-        @Override
-        public void onClick(View v) {
-            setContentView(R.layout.stand_alone);
-            StandAloneRunnable TC = null;
-            try {
-                TC = new StandAloneRunnable(getApplicationContext(), mActivity, (SeekBar) findViewById(R.id.seekBar),
-                        (FrameLayout) findViewById(R.id.camera_preview), (TextView) findViewById(R.id.textView), (TextView) findViewById(R.id.textView3), (ToggleButton) findViewById(R.id.toggleButton));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            TC.start();
-        }
-    }
-
 }
 
 
