b87a13939f41b70a908b62837b307f79c29e0c24
elrhino <renlane@hotmail.com>
2015-02-23 15:31:13 -0500

    Fix mineur

--- Droid/app/src/main/java/stevenseesall/com/camera/MainActivity.java
+++ Droid/app/src/main/java/stevenseesall/com/camera/MainActivity.java
@@ -7,10 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.ToggleButton;
-import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity{
 
@@ -18,32 +14,35 @@ public class MainActivity extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        findViewById(R.id.btn_Connect).setOnClickListener(new Connection_OnClickListener(this));
+        findViewById(R.id.btn_Connect).setOnClickListener(
+            new Connection_OnClickListener(this)
+        );
     }
 
     private class Connection_OnClickListener implements View.OnClickListener {
 
-        Activity mActivity;
-        EditText mServerIPField = (EditText)findViewById(R.id.txtIp);
-        FrameLayout mCameraPreview;
+        private Activity mActivity;
+        private EditText mServerEditText;
+        private FrameLayout mCameraPreview;
 
         public Connection_OnClickListener(Activity activity) {
             mActivity = activity;
+            mServerEditText = (EditText)findViewById(R.id.txtIp);
         }
 
         @Override
         public void onClick(View v) {
             setContentView(R.layout.remote_mode);
+            String encryptedAddress = mServerEditText.getText().toString();
+            String decryptedAddress = IPAddressCipher.decryptIPAddress(encryptedAddress);
             mCameraPreview = (FrameLayout)findViewById(R.id.camera_preview);
-
-            String serverIp = mServerIPField.getText().toString();
-            serverIp = IPAddressCipher.decryptIPAddress(serverIp);
             UDPRunnable udpRunnable = new UDPRunnable(
                     getApplicationContext(),
                     mActivity,
                     mCameraPreview,
-                    serverIp
+                    decryptedAddress
             );
+
             udpRunnable.start();
         }
     }
