16e934b77f0ea816017818bc2977cf619296a4b9
elrhino <renlane@hotmail.com>
2015-02-27 13:24:50 -0500

    Modifier la classe pour accepter le nouveau format de connectrionString

--- Droid/app/src/main/java/stevenseesall/com/camera/IPAddressCipher.java
+++ Droid/app/src/main/java/stevenseesall/com/camera/IPAddressCipher.java
@@ -18,36 +18,60 @@ public class IPAddressCipher {
     }
 
     public String decryptIPAddress(String serverAddress) {
-
         String gatewayAddr = Formatter.formatIpAddress(mGateway);
-        String[] splittedAddr = serverAddress.split("\\.");
-        String[] splittedGateway = gatewayAddr.split("\\.");
-        String[] splittedFinalAddr = new String[4];
+        String[] connectionStringArray = splitEveryThirdCharacter(serverAddress);
+        String[] gatewayAddressArray = gatewayAddr.split("\\.");
+        String[] hostAddressArray = new String[4];
 
         String ipAddress = "";
 
-        int addrLen = splittedAddr.length;
+        int addrLen = connectionStringArray.length;
         int gatewayLen = 4 - addrLen;
         int j = 0;
 
         for (int i = 0; i < gatewayLen; i++) {
-            splittedFinalAddr[i] = splittedGateway[i];
+            hostAddressArray[i] = gatewayAddressArray[i];
         }
 
         for (int i = gatewayLen; i < 4; i++) {
-            String st = splittedAddr[j];
-            int dec = Integer.parseInt(st, 16);
-            String sDec = Integer.toString(dec);
-            splittedFinalAddr[i] = sDec;
+            String st = connectionStringArray[j];
+            st = removeFilling(st);
+            hostAddressArray[i] = st;
             j++;
         }
 
-        for (String st: splittedFinalAddr) {
+        for (String st: hostAddressArray) {
             ipAddress = ipAddress + '.' + st;
         }
 
-        ipAddress = ipAddress.substring(1, ipAddress.length());
-
         return ipAddress;
+    }
+
+    private String[] splitEveryThirdCharacter(String s) {
+        String[] sArray;
+        int length;
+
+        if (s.length() % 3 != 0) {
+            System.console().printf("Connection string is not a multiple of 3.");
+        }
+
+        length = s.length() / 3;
+        sArray = new String[length];
+        int j = 0;
+
+        for (int i = 0; i < length; i++) {
+            sArray[i] = s.substring(j, j + 3);
+            j += 3;
+        }
+
+        return sArray;
+    }
+
+    private String removeFilling(String s) {
+        while (s.length() > 1 && s.startsWith("0")) {
+            s = s.substring(1, s.length());
+        }
+
+        return s;
     }
 }
